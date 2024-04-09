765d7095629f89801eb57cf44f4ef21e27e2dd25
NlodeL <nlodelop@myuax.com>
2024-04-09 19:41:43 +0200

    creo la clase contacto para guararlos mediante la agenda

---
+++ src/main/java/AgendayBuscador/Contacto.java
@@ -0,0 +1,37 @@
+package AgendayBuscador;
+
+public class Contacto {
+    private String nombre;
+    private String email;
+    private String numeroTelefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public Contacto(String nombre, String email, String numeroTelefono) {
+        this.nombre = nombre;
+        this.email = email;
+        this.numeroTelefono = numeroTelefono;
+    }
+}
